0c1305f17dc28be8a2f975db3488ce70a08c83dc
ARuffy <aershov.ge@gmail.com>
2024-10-15 23:29:39 -0500

    [Engine] Add RuntimeClass

--- Engine/Include/Sorex/Types.h
+++ Engine/Include/Sorex/Types.h
@@ -19,99 +19,100 @@
 // TODO: debug:
 #define srx_inline inline
 #define srx_noexcept noexcept
+#define srx_consteval consteval
 #define srx_nodiscard [[nodiscard]]
 
 #define SRX_IDLE \
-    do           \
-    {            \
-    } while (0)
+  do             \
+  {              \
+  } while (0)
 
 namespace Sorex
 {
-    typedef std::int8_t  int8;
-    typedef std::int16_t int16;
-    typedef std::int32_t int32;
-    typedef std::int64_t int64;
+  typedef std::int8_t  int8;
+  typedef std::int16_t int16;
+  typedef std::int32_t int32;
+  typedef std::int64_t int64;
 
-    typedef unsigned char byte;
+  typedef unsigned char byte;
 
-    typedef std::uint8_t  uint8;
-    typedef std::uint16_t uint16;
-    typedef std::uint32_t uint32;
-    typedef std::uint64_t uint64;
+  typedef std::uint8_t  uint8;
+  typedef std::uint16_t uint16;
+  typedef std::uint32_t uint32;
+  typedef std::uint64_t uint64;
 
-    typedef std::intptr_t intptr_t;
+  typedef std::intptr_t intptr_t;
 
-    typedef std::size_t size_t;
-    typedef size_t      hash_t;
+  typedef std::size_t size_t;
+  typedef size_t      hash_t;
 
-    typedef int errno_t;
+  typedef int errno_t;
 
-    template<typename T>
-    using THash = std::hash<T>;
+  template<typename T>
+  using THash = std::hash<T>;
 
-    typedef std::string  String;
-    typedef std::wstring WString;
+  typedef std::string  String;
+  typedef std::wstring WString;
 
-    template<typename Char>
-    using BasicString = std::basic_string<Char>;
+  template<typename Char>
+  using BasicString = std::basic_string<Char>;
 
-    typedef std::string_view  StrView;
-    typedef std::wstring_view WStrView;
+  typedef std::string_view  StringView;
+  typedef std::wstring_view WStringView;
 
-    template<typename Char>
-    using BasicStringView = std::basic_string_view<Char>;
+  template<typename Char>
+  using BasicStringView = std::basic_string_view<Char>;
 
-    template<typename T>
-    using TVector = std::vector<T>;
-    template<typename T, size_t N>
-    using TArray = std::array<T, N>;
-    template<typename T>
-    using TStack = std::stack<T>;
-    template<typename T>
-    using TQueue = std::queue<T>;
-    template<typename T>
-    using TPriorityQueue = std::priority_queue<T>;
-    template<typename T>
-    using TSpan = std::span<T>;
+  template<typename T>
+  using TVector = std::vector<T>;
+  template<typename T, size_t N>
+  using TArray = std::array<T, N>;
+  template<typename T>
+  using TStack = std::stack<T>;
+  template<typename T>
+  using TQueue = std::queue<T>;
+  template<typename T>
+  using TPriorityQueue = std::priority_queue<T>;
+  template<typename T>
+  using TSpan = std::span<T>;
 
-    template<typename T>
-    using TSet = std::set<T>;
-    template<typename T, typename Hasher = std::hash<T>>
-    using THashSet = std::unordered_set<T, Hasher>;
+  template<typename T>
+  using TSet = std::set<T>;
+  template<typename T, typename Hasher = std::hash<T>>
+  using THashSet = std::unordered_set<T, Hasher>;
 
-    template<typename TKey, typename TValue>
-    using TMap = std::map<TKey, TValue>;
-    template<typename TKey, typename TValue>
-    using THashMap = std::unordered_map<TKey, TValue>;
+  template<typename TKey, typename TValue>
+  using TMap = std::map<TKey, TValue>;
+  template<typename TKey, typename TValue>
+  using THashMap = std::unordered_map<TKey, TValue>;
 
-    template<typename T>
-    using TList = std::list<T>;
-    template<typename T>
-    using TLinkedList = std::forward_list<T>;
+  template<typename T>
+  using TList = std::list<T>;
+  template<typename T>
+  using TLinkedList = std::forward_list<T>;
 
-    template<typename TFirst, typename TSecond>
-    using TPair = std::pair<TFirst, TSecond>;
+  template<typename TFirst, typename TSecond>
+  using TPair = std::pair<TFirst, TSecond>;
 
-    template<typename T>
-    using TUniquePtr = std::unique_ptr<T>;
-    template<typename T>
-    using TSharedPtr = std::shared_ptr<T>;
-    template<typename T>
-    using TWeakPtr = std::weak_ptr<T>;
+  template<typename T>
+  using TUniquePtr = std::unique_ptr<T>;
+  template<typename T>
+  using TSharedPtr = std::shared_ptr<T>;
+  template<typename T>
+  using TWeakPtr = std::weak_ptr<T>;
 
-    template<typename T, typename... Args>
-    srx_nodiscard srx_inline TUniquePtr<T> MakeUnique(Args&&... args)
-    {
-        return std::make_unique<T>(std::forward<Args>(args)...);
-    }
+  template<typename T, typename... Args>
+  srx_nodiscard srx_inline TUniquePtr<T> MakeUnique(Args&&... args)
+  {
+    return std::make_unique<T>(std::forward<Args>(args)...);
+  }
 
-    template<typename T, typename... Args>
-    srx_nodiscard srx_inline TSharedPtr<T> MakeShared(Args&&... args)
-    {
-        return std::make_shared<T>(std::forward<Args>(args)...);
-    }
+  template<typename T, typename... Args>
+  srx_nodiscard srx_inline TSharedPtr<T> MakeShared(Args&&... args)
+  {
+    return std::make_shared<T>(std::forward<Args>(args)...);
+  }
 
-    template<typename... Args>
-    using TVariant = std::variant<Args...>;
+  template<typename... Args>
+  using TVariant = std::variant<Args...>;
 }
