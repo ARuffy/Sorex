c8de4493cbcb6a6a7540cd64625e393d857f3ba5
ARuffy <aershov.ge@gmail.com>
2024-10-23 22:45:28 -0500

    Fix file formatting

--- Engine/Include/Sorex/Assert.h
+++ Engine/Include/Sorex/Assert.h
@@ -41,8 +41,7 @@ namespace Sorex::Platform
    *
    * @return 0 - to continue with assert(expr) to terminate application;
    */
-  int OnAssertionFailed(const char* message, 
-                        const char* file, int line);
+  int OnAssertionFailed(const char* message, const char* file, int line);
 
   /**
    * @brief OnCheckFailed - called when check/verify expression failed.
